b2abaec0ebed6c7e2f68eb8d2b5688b64d1222d9
devthigas <thi.deusefiel@gmail.com>
2022-07-28 15:47:18 -0300

    finalizando falta otimizar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,14 @@
+
+// Objeto da biblioteca do typeit. Recebe o id do Dom, a mensagem para a animação, 
+// o tempo da mensagem, e se ela ficará visivel ou não.
+
 new TypeIt("#animate", {
-    strings: " Bem-vindo ao meu portifolio, desenvolvo aplicações utilizando HTML, CSS e JavaScript.",
+    strings: " Bem-vindo ao meu portifólio! Desenvolvo aplicações utilizando HTML, CSS e JavaScript.",
     speed: 100,
     waitUntilVisible: true,
 }).go();
+
+// Enviar alerta ao clicar no botão 
+function btnMsg(){
+    alert("Formulario em construção")
+}
